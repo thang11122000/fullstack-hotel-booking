13dc8bb07dd62a8932d802bc79b38a5bfe82627b
Doan Thang <thangnd111220@gmail.com>
2025-07-31 17:51:57 +0700

    Add AllRooms and RoomDetail pages, and integrate them into the main application

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import { Route, Routes, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
+import AllRooms from "./pages/AllRooms";
+import RoomDetail from "./pages/RoomDetail";
 
 const App = () => {
   const isOwnerPath = useLocation().pathname.includes("owner");
@@ -12,6 +14,8 @@ const App = () => {
       <div className="min-h-[70vh]">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/rooms" element={<AllRooms />} />
+          <Route path="/rooms/:id" element={<RoomDetail />} />
         </Routes>
       </div>
     </div>
